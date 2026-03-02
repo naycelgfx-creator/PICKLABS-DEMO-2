16b40b30ba5b0d3b83c896bdba82803e248d99ee
Edwin Garcia <naycelgfx@Edwins-MacBook-Pro.local>
2026-03-02 00:05:53 -0500

    chore: Tune chunk size limit and setup manualChunks for Vite to fix Vercel warnings

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,28 @@ export default defineConfig({
   server: {
     host: true,
   },
+  build: {
+    chunkSizeWarningLimit: 4000,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'react-vendor';
+            }
+            if (id.includes('framer-motion')) {
+              return 'framer-motion';
+            }
+            if (id.includes('lucide-react')) {
+              return 'lucide-react';
+            }
+            if (id.includes('recharts')) {
+              return 'recharts';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
